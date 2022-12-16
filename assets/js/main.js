de02420f59fa34f8df64d172666873d09dcd407f
farid <siteteme@gmail.com>
2022-12-16 12:56:28 +0330

    header & navbar is added

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+let chevron = document.querySelectorAll('.chevron01');
+let chevron01 = document.querySelectorAll('.chevron01 img');
+let subMenu = document.querySelectorAll('.sub-nav-items')
+
+
+
+function showSubMenu() {
+    subMenu.forEach(item=>{
+        if (item.style.display === "none"){
+            item.style.display="grid"
+            chevron01.forEach(item=>{
+                item.addEventListener("click",()=>{
+                    item.style.transform = 'rotate(0)';
+                })
+            })
+        }else {
+            item.style.display="none";
+            chevron01.forEach(item=>{
+                item.addEventListener("click",()=>{
+                    item.style.transform = 'rotate(180deg)';
+                })
+            })
+        }
+    })
+
+}
+
+chevron.forEach(item=>{
+    item.addEventListener("click",showSubMenu)
+})
+
+
